a87eb5fc0b211f76e9d6c874f0023766719a54c0
Timothy Mubiru <mubstimor@gmail.com>
2019-03-22 12:40:38 +0300

    ft(interface): connect details layout
    - add recycler adapter to main file
    - pass intent to detail activity
    - add picasso library
    - display image from remote url
    - display image in a circle with CircleImageView
    
    [Delivers #155820624]

---
+++ app/src/main/java/mubstimor/android/codelab/GithubUserAdapter.java
@@ -0,0 +1,92 @@
+package mubstimor.android.codelab;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class GithubUserAdapter extends RecyclerView.Adapter<GithubUserAdapter.UserViewHolder> {
+
+    private ListItemClickListener mOnClickListener;
+
+    private Context context;
+
+    private ArrayList<GithubUser> list_members=new ArrayList<>();
+
+    public interface ListItemClickListener {
+        void onListItemClick(int clickedItemIndex);
+    }
+
+    public GithubUserAdapter(ListItemClickListener listener) {
+        mOnClickListener = listener;
+    }
+
+    @Override
+    public UserViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        context = viewGroup.getContext();
+        int layoutIdForListItem = R.layout.user_list_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+
+        View view = inflater.inflate(layoutIdForListItem, viewGroup, shouldAttachToParentImmediately);
+        UserViewHolder viewHolder = new UserViewHolder(view);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(UserViewHolder holder, int position) {
+        int backgroundColorForViewHolder = ColorUtils
+                .getViewHolderBackgroundColorFromInstance(context, position);
+        holder.itemView.setBackgroundColor(backgroundColorForViewHolder);
+        GithubUser list_items = list_members.get(position);
+        holder.usernameView.setText(list_items.getUsername());
+        Picasso.with(context).load(list_items.getImageUrl()).into(holder.userImageView);
+    }
+
+
+    /**
+     * set array list
+     * */
+    public void setListContent(ArrayList<GithubUser> list_members) {
+
+        this.list_members = list_members;
+        notifyItemRangeChanged(0, list_members.size());
+    }
+
+    @Override
+    public int getItemCount() {
+        return list_members.size();
+    }
+
+
+    class UserViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        TextView usernameView;
+        CircleImageView userImageView;
+
+        public UserViewHolder(View itemView) {
+            super(itemView);
+
+            usernameView = (TextView) itemView.findViewById(R.id.tv_username);
+            userImageView = (CircleImageView) itemView.findViewById(R.id.iv_user_image);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            int clickedPosition = getAdapterPosition();
+            mOnClickListener.onListItemClick(clickedPosition);
+        }
+
+    }
+}
---
+++ app/src/main/java/mubstimor/android/codelab/UserDetailActivity.java
@@ -0,0 +1,37 @@
+package mubstimor.android.codelab;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class UserDetailActivity extends AppCompatActivity {
+
+    Intent intent;
+    TextView usernameTextView;
+    CircleImageView userImageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_detail);
+
+        intent = getIntent();
+        usernameTextView = (TextView) findViewById(R.id.tv_full_name);
+        userImageView = (CircleImageView) findViewById(R.id.iv_user_image);
+
+        String username = intent.getStringExtra("user");
+        String imageUrl = intent.getStringExtra("image");
+        Picasso.with(UserDetailActivity.this).load(imageUrl).into(userImageView);
+
+        usernameTextView.setText(username);
+
+    }
+}
